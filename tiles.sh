846b59e7b6d6b23b20e97d470b8b648ccb0db2bc
Michael Kennedy <michaelkennedyns5@gmail.com>
2020-04-09 13:20:30 -0700

    Fixing bug with update tiles.txt

--- tiles.sh
+++ tiles.sh
@@ -34,12 +34,12 @@ if [ ${#tiles} -eq 0 ]; then
    read -p "Do you want to create the file? (Y || N) " ans
    shopt -s nocasematch
    getLines
-	if [ $ans -eq "Y" ] && [ $?-eq 3 ]; then
+	if [ $ans == "Y" ] && [ $? -eq 3 ]; then
          adb shell "settings get secure sysui_qs_tiles" > $path/tiles.txt
          echo "Created text file. Please re-start this program to load quicktiles"
          exit 0
-	elif [ $ans -eq "N" ] || [ $ans -eq "Y" ] && [ $? -ne 3]; then
-	echo "Device is not connected"
+   elif [ $ans ==  "N" ] || [ $ans -eq "Y" ] && [ $? -ne 3]; then
+	   echo "Device is not connected"
        exit 0
    fi
 fi
